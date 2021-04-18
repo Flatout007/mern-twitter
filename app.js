695f00c11b72a76c072d8a3e37cb1ea1d9160d26
Nightwatch <rdmc0072@yahoo.com>
2021-04-18 09:11:37 -0700

    completed user auth routes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const db = require('./config/keys').mongoURI;
+const passport = require('passport');
+
 
 const app = express();
 
@@ -14,17 +16,26 @@ mongoose
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
+
+// app.get("/", (req, res) => {
   
-    console.log(res)
+//     console.log(res)
     
-    res.send("wassup")
+//     res.send("wassup")
 
 
-});
+// });
+
+// user auth route
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({ msg: 'Success' });
+})
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(passport.initialize());
+require('./config/passport')(passport);
