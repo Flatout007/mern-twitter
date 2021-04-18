d98ca95757c25bbbf1fb9716b878d71e56e6c1bc
Nightwatch <rdmc0072@yahoo.com>
2021-04-17 21:53:01 -0700

    created server and routes

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const mongoose = require('mongoose');
 const express = require('express')
-const app = express();
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 const db = require('./config/keys').mongoURI;
+
+const app = express();
+
 mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true }) 
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("wassup"));
+app.get("/", (req, res) => {
+  
+    console.log(res)
+    
+    res.send("wassup")
+
+
+});
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
